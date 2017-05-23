a56fad9eecc3f7618190758dde3f61373fd3e046
Adam Nicholson <adamnicholson10@gmail.com>
2017-05-23 09:56:48 +0100

    Improve docblock typehints

--- src/Repository/Repository.php
+++ src/Repository/Repository.php
@@ -10,32 +10,32 @@ interface Repository
     /**
      * Save a rule
      *
-     * @param $identity
-     * @param $role
-     * @param $context
+     * @param string $identity
+     * @param string $role
+     * @param string $context
      * @param string $state STATE_ALLOW or STATE_DENY
      * @return void
      */
     public function saveRule($identity, $role, $context, $state);
 
     /**
-     * @param $identity
-     * @param $role
-     * @param $context
+     * @param string $identity
+     * @param string $role
+     * @param string $context
      * @return string|null
      */
     public function getRuleState($identity, $role, $context);
 
     /**
-     * @param $role
-     * @param $parent
+     * @param string $role
+     * @param string $parent
      * @return void
      */
     public function saveRole($role, $parent);
 
     /**
-     * @param $role
-     * @return array|null
+     * @param string $role
+     * @return string|null
      */
     public function getRoleParent($role);
 
